1bbf26c41d69a8a8033bad567c6fca416f67f299
Mike Hawes <mhawes24@gmail.com>
2017-11-26 14:27:42 -0500

    Added hammer.js for component gestures. Added Angular material. Added browser animations. Added angular animations. Added material iconst to index.html

--- app/static/src/app/app.module.ts
+++ app/static/src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import 'hammerjs';
+import { RouterModule, Routes } from '@angular/router';
+
+
+const appRoutes: Routes = [
+    {
+        path      : '**',
+        //comonent: [YourImportedComponentClass]
+    },
+
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
